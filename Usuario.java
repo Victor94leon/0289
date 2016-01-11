e90694d83572cc597e600f871173ea656bfaf0e5
Víctor <victorlopez94leon@gmail.com>
2016-01-11 12:20:11 +0100

    Añadido método para comparar las calorías ingeridas entre dos usuarios

--- Usuario.java
+++ Usuario.java
@@ -2,8 +2,6 @@ public class Usuario
 {
     // String para guardar el nombre del usuario
     private String nombreUsuario;
-    // Atributo de tipo Alimento para guardar la información del alimento que come el usuario
-    private Alimento alimentoQueCome;
     // Float que guarda la cantidad de proteinas ingeridas 
     private float proteinasTotales;
     // Float que guarda la cantidad de carbohidratos ingeridos
@@ -12,7 +10,7 @@ public class Usuario
     private float grasasTotales;
     // Float que guarda la cantidad de calorías totales
     private float caloriasTotales;
-    
+
     /**
      * Constructor for objects of class Usuario
      */
@@ -24,7 +22,7 @@ public class Usuario
         grasasTotales = 0;
         caloriasTotales = 0;
     }
-    
+
     /**
      * Método para simular que el usuario coma una cantidad de alimento
      */
@@ -35,7 +33,7 @@ public class Usuario
         grasasTotales = grasasTotales + (alimentoQueCome.getGrasas() / 100 * gramosDeAlimentoQueCome);
         caloriasTotales = caloriasTotales + (alimentoQueCome.getCalorias() / 100 * gramosDeAlimentoQueCome);
     }
-    
+
     /**
      * Método quemuestra por pantalla el estado del usuario mostando las cantidades ingeridas
      */
@@ -47,5 +45,38 @@ public class Usuario
         System.out.println("Gramos totales de carbohidratos ingeridos: " + carbohidratosTotales + "(" + (carbohidratosTotales  / alimentoTotal * 100) + "%)");
         System.out.println("Gramos totales de grasas ingeridos:        " + grasasTotales + "(" + (grasasTotales / alimentoTotal * 100 ) + "%)");
         System.out.println("Calorias totales ingeridas:                " + caloriasTotales);
+    }
+
+    /**
+     * Método getter que devuelve las calorias que ha ingerido el usuario
+     */
+    public float getCaloriasTotales()
+    {
+        return caloriasTotales;
+    }
+
+    /**
+     * Método getter que devuelve el nombre del usuario
+     */
+    public String getNombreUsuario()
+    {
+        return nombreUsuario;
+    }
+
+    /**
+     * Método que permite comparar la ingesta de calorías con otro usuario
+     */
+    public void comparar(Usuario personaParaComparar)
+    {
+        String comparacion = " ha ingerido menos calorias que "; 
+        if (caloriasTotales == personaParaComparar.getCaloriasTotales()) {
+            comparacion = " ha ingerido las mismas calorías que ";
+        }
+        else if (caloriasTotales > personaParaComparar.getCaloriasTotales()) {
+            comparacion = " ha ingerido más calorías que ";
+        }
+        System.out.println
+            (nombreUsuario + comparacion + personaParaComparar.getNombreUsuario() + 
+            "(" + caloriasTotales + " frente a " + personaParaComparar.getCaloriasTotales() + ")");
     }
 }
